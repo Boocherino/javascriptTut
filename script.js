2e753b7d6ba2ffe84b04ee7fbfc10d2fec9815eb
Boocherino <dtbucci@coastal.edu>
2023-11-29 13:36:54 -0500

    added a quote rate system and shows all ratings at the end

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var ratings = {};
+var author;
+
+function rateQuote(author, rating) {
+    ratings[author] = rating;
+}
+
 function generate() {
     var quotes = {
         "- Michelangelo": '"I saw the angel in the marble and carved until I set him free."',
@@ -6,13 +13,63 @@ function generate() {
         "- Stephen King": '"Get busy living or get busy dying."',
     }
 
-    var authors = Object.keys(quotes); // returns an array of keys
+    var authors = Object.keys(quotes);
 
-    var author = authors[Math.floor(Math.random() * authors.length)]; // random key from authors array
-    //console.log(authors);
+    author = authors[Math.floor(Math.random() * authors.length)];
 
-    var quote = quotes[author]; // value of the random key
+    var quote = quotes[author];
 
     document.getElementById("quote").innerHTML = quote;
     document.getElementById("author").innerHTML = author;
+
+    var rating = ratings[author];
+    if (rating) {
+        document.getElementById("rating").innerHTML = "Rating: " + rating;
+    } else {
+        document.getElementById("rating").innerHTML = "Not rated yet";
+    }
 }
+
+document.getElementById('rating-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    if (!author) {
+        alert("Please generate a quote before rating.");
+        return;
+    }
+    var rating = document.querySelector('input[name="rating"]:checked').value;
+
+    rateQuote(author, rating);
+
+    alert("You rated the quote by " + author + " a " + rating + "/5.");
+
+    generate();
+});
+
+function showRatings() {
+    var ratingsString = "";
+    for (var author in ratings) {
+        ratingsString += "Quote by " + author + ": " + ratings[author] + "/5\n";
+    }
+    alert(ratingsString);
+}
+
+document.getElementById('rating-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    if (!author) {
+        alert("Please generate a quote before rating.");
+        return;
+    }
+    var rating = document.querySelector('input[name="rating"]:checked').value;
+
+    rateQuote(author, rating);
+
+    alert("You rated the quote by " + author + " a " + rating + "/5.");
+
+    generate();
+
+    if (Object.keys(ratings).length === Object.keys(quotes).length) {
+        showRatings();
+    }
+});
